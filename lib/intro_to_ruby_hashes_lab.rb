f82d4493fe7872a7552765ede213f851c828adab
immutable-hashmap-7791 <abboudahmed1@gmail.com>
2020-08-07 21:12:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,11 +1,13 @@
 def new_hash
   # return an empty hash
-  {}
+  hash = {}
+  hash
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  hi: "are you here?"
+ hash = {hi: "are you here?"}
+ hash
 end
 
 def pioneer
